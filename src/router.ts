6b70aced776e1ecaeaf76db74a6babb86379a456
Nanami Nakano <64841155+NanamiNakano@users.noreply.github.com>
2025-01-03 23:13:29 +0800

    feat: limit some command to be used in private chat only

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,9 @@ export const router = new Router<MyContext>(async (ctx) => {
       await ctx.reply("You are not allowed to use this command.");
       return;
     }
+    if (ctx.chat.type != "private") {
+      return;
+    }
 
     return "command:add";
   }
@@ -41,12 +44,18 @@ export const router = new Router<MyContext>(async (ctx) => {
       await ctx.reply("You are not allowed to use this command.");
       return;
     }
+    if (ctx.chat.type != "private") {
+      return;
+    }
 
     return "command:whitelist";
   }
   if (ctx.hasCommand("blacklist")) {
     if (ctx.from?.id != config.owner) {
       await ctx.reply("You are not allowed to use this command.");
+      return;
+    }
+    if (ctx.chat.type != "private") {
       return;
     }
 
